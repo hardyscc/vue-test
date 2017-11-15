a8b4f00072013384dd3e5202e1307a5663098dc0
Hardys <hardyscc@gmail.com>
2017-11-15 17:16:14 +0800

    add bulma

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,6 @@
+import 'bulma/css/bulma.css'
+import 'font-awesome/css/font-awesome.min.css'
+
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
